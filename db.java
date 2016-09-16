11bc0f57eb323b0496460cadac324fdca71e1762
BeholdTheDevil <antdeg@outlook.com>
2016-09-16 13:04:15 +0200

    Addition of a GUI as well as password handling changes
    A whole new part of the project has been started. I am now trying to incorporate the previously command line run program into a JFrame based GUI. 
    Also rewrote the password hadnling so that it could be handled through the GUI as well as the commandline(through crawler.java).
    --To do--
    * Continue to piece together the crawler.java code with the new crawlerGUI.java code.
    * Make sure that the password handling is correct. Look at the line "public static db data = new db();" at stat of files: crawler.java and fetch.java

--- db.java
+++ db.java
@@ -4,22 +4,34 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import javax.sql.DataSource;
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class db {
 	public Connection conn = null;
+	public MysqlDataSource ds = null;
 
-	public db() {
+	public db(String inputPass) {
+		
 		Console console = System.console();
+		ds = new MysqlDataSource();
+		ds.setURL("jdbc:mysql://localhost:3306/Crawler?autoReconnectForPools=true&useSSL=false");
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
-			String url = "jdbc:mysql://localhost:3306/Crawler?autoReconnect=true&useSSL=false";
-			char pass[] = console.readPassword(" Enter pass: ");
-
-			conn = DriverManager.getConnection(url, "root",  new String(pass));
+			String url = "jdbc:mysql://localhost:3306/Crawler?autoReconnectForPools=true&useSSL=false";
+			if(inputPass.length() == 0) {
+				inputPass = new String(console.readPassword(" Enter password: "));
+			}
+			//conn = DriverManager.getConnection(url, "root",  inputPass);
+			conn = ds.getConnection("root", inputPass);
 			System.out.println(" Connection established");
+		} catch (SQLException passexc) {
+			System.out.println(" Invalid Password");
+		} catch (NullPointerException nullexc) {
+			throw nullexc;
 		} catch (Exception e) {
 			e.printStackTrace();
-		}
+		} 
 	}
 
 	public ResultSet runSql(String sql) throws SQLException {
